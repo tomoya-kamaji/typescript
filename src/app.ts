9fa3d16f9b869efb0aaf22b2c10ab7d37f557853
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-11-07 11:04:17 +0900

    learn type

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+const userName = 'Max';
+// userName = 'Maximilian';
+let age = 30;
+
+age = 29;
+
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
+
+// if (age >= 20) {
+//   let isAdult = true;
+// }
+
+// console.log(isAdult);
+
+//console.log(result);
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (output: string | number) => void = output => {
+  console.log(output);
+};
+
+printOutput(add(2, 5));
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => {
+    console.log(event);
+  });
+}
