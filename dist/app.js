97cc9256f31bc561170ee3073e62e8a0cc82b56b
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-11-14 11:07:02 +0900

    commit

--- dist/app.js
+++ dist/app.js
@@ -1,81 +1,58 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employees = [];
+const e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
     }
-    static createEmployee(name) {
-        return { name: name };
+    return a + b;
+}
+function printEmployeeInfomation(emp) {
+    console.log(emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges:" + emp.privileges);
     }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
+    if ("startDate" in emp) {
+        console.log("Start Date:" + emp.startDate);
     }
 }
-Department.fiscalYear = 2020;
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, "IT");
-        this.admins = admins;
-    }
-    describe() {
-        console.log('IT部門-ID:' + this.id);
+printEmployeeInfomation({ name: "Menu", startDate: new Date() });
+class Car {
+    drive() {
+        console.log("運転中,,,");
     }
 }
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, "Accounting");
-        this.reports = reports;
-        this.lastReport = reports[0];
+class Truck {
+    drive() {
+        console.log("トラック運転中,,,");
     }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error("レポートが見つかりません。");
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error("正しい値を設定してください。");
-        }
-        this.addReport(value);
-    }
-    describe() {
-        console.log("会計部門 - ID:" + this.id);
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-    addEmployee(name) {
-        if (name === "Max") {
-            return;
-        }
-        this.employees.push(name);
+    loadCargo(amount) {
+        console.log("荷物を載せています..." + amount);
     }
 }
-const employee1 = Department.createEmployee('Max');
-console.log(employee1, Department.fiscalYear);
-const it = new ITDepartment("d1", ["Max"]);
-it.addEmployee("Max");
-it.addEmployee("Manu");
-it.describe();
-it.printEmployeeInformation();
-console.log(it);
-const accounting = new AccountingDepartment("d2", []);
-console.log(accounting);
-accounting.mostRecentReport = "通期会計レポート";
-accounting.addReport("Something");
-console.log(accounting.mostRecentReport);
-accounting.printReports();
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-accounting.printEmployeeInformation();
+const v1 = new Car();
+const v2 = new Truck();
+function userVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+userVehicle(v1);
+userVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log("移動速度：" + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
 //# sourceMappingURL=app.js.map
